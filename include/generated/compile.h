a8b4f435b0651cda3274a1c9e64544fb209b549d
BAYOO1 <architectforever100@gmail.com>
2016-12-15 12:16:10 +0000

    initial

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Dec 8 11:57:12 GMT 2016"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "architect-desktop"
+#define LINUX_COMPILER "gcc version 4.9.4 20160303 (optimized) (UBERTC-4.9.4) "
